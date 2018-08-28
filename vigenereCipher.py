916a238277f56438d7976011a030b47ea5bebe26
root <root@localhost.localdomain>
2018-08-28 08:27:05 +0530

    nn

--- vigenereCipher.py
+++ vigenereCipher.py
@@ -1,1 +1,12 @@
-print("real")
+def vigenereCipher(key,message):
+    output=""
+    print(message)
+    print(key)
+    for i in range(len(message)):
+        print("the char is:",chr(ord(key[i%len(key)])))
+       #print(chr(((ord(message[i])+ord(key[i%len(key)])-ord("a")))))
+        output=output+(chr(((ord(message[i])+ord(key[i%len(key)])-ord("a")))))
+       # print(output)
+    print("theputis:",output)
+
+vigenereCipher("kkkss","mohantejaisagoodboy")
