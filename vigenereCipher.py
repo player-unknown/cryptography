796924a1bc37d9f3cb7222b651b85011aa5c0caa
player-unknown <player-unknown@localhost.localdomain>
2018-10-03 20:27:59 +0530

    added decipher function

--- vigenereCipher.py
+++ vigenereCipher.py
@@ -1,12 +1,38 @@
 def vigenereCipher(key,message):
     output=""
-    print(message)
     print(key)
+    print()
+    print(message)
     for i in range(len(message)):
-        print("the char is:",chr(ord(key[i%len(key)])))
+       # print("the char is:",chr(ord(key[i%len(key)])))
        #print(chr(((ord(message[i])+ord(key[i%len(key)])-ord("a")))))
-        output=output+(chr(((ord(message[i])+ord(key[i%len(key)])-ord("a")))))
-       # print(output)
-    print("theputis:",output)
+       #what does it do
+       # print(message[i] +"=" + str(ord(message[i])))
+        output=output+(chr(((ord(message[i])+ord(key[i%len(key)])-2*ord("a")))%26+ord("a")))
+        #print(output)
+    #print("the output is:",output)
+    return output
 
-vigenereCipher("kkkss","mohantejaisagoodboy")
+
+def vigenereDeCipher(key,encryptedMessage):
+    print("key : " + key)
+    print("encrypted message is:"+ encryptedMessage)
+    message=""
+    for i in range(len(encryptedMessage)):
+        print(chr(ord("a")+i))
+        print(encryptedMessage[i],str(ord(encryptedMessage[i])))
+        print(key[i%len(key)] , str(ord(key[i%len(key)])))
+        n=ord(encryptedMessage[i])-(ord(key[i%len(key)])-ord("a"))
+       
+        if(n<ord("a")):
+            n=ord("a")+(ord("a")-n)
+
+        print(n)
+        print(chr(n))
+        print()
+        message=message + chr(n) 
+    print(message)    
+output=vigenereCipher("hjikl","abcdefghijklmnopqrstuvwxyz")
+print(output)
+print()
+vigenereDeCipher("hjikl",output )
